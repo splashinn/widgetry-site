1ff3daa75efc21c0e9835d5d0ade1ad7a1d3cb92
kyle <the.kyle.maune@gmail.com>
2017-07-14 14:16:11 -0700

    added pages for about, locations, start a project, contact

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,24 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
-  
+
   def home
 
+  end
+
+  def about
+
+  end
+
+  def locations
+
+  end
+
+  def contact_us
+
+  end
+
+  def hire_us
+    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home', id: 'home'
+
+  get 'pages/about', id: 'about'
+  get 'pages/locations', id: 'location'
+  get 'pages/contact_us', id: 'contact_us'
+  get 'pages/hire_us', id: 'hire_us'
 end
